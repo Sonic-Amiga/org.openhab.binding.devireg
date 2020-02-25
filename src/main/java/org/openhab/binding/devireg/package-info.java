843b1ed08e180ace0468a1a8a2163760e2bfd659
Pavel Fedin <pavel_fedin@mail.ru>
2020-02-25 22:22:35 +0300

    Add native libraries
    Add native libraries for ARM32 and Win64. The set is incomplete, for testing.

---
+++ src/main/java/org/openhab/binding/devireg/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2010-2020 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+@org.osgi.annotation.bundle.Header(name = org.osgi.framework.Constants.BUNDLE_NATIVECODE, value = "jni/win64/opensdg_jni.dll;processor=amd64;osname=win32,jni/armhf/libopensdg_jni.so;processor=arm;osname=linux")
+package org.openhab.binding.devireg;
+
+/**
+ * Additional information for BlueZ package
+ *
+ * @author Jan N. Klug - Initial contribution
+ *
+ */
